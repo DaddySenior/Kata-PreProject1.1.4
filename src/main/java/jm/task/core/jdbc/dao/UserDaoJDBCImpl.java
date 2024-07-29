2b252b5f0328c9b144baeccb8711c60bb17b8e53
DaddySenior <vinny2005@mail.ru>
2024-07-29 03:28:02 +0300

    code style

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -15,68 +15,72 @@ public class UserDaoJDBCImpl implements UserDao {
     public void createUsersTable() {
         Connection connection = Util.getConnection();
         try (Statement statement = connection.createStatement()) {
-            statement.executeUpdate("CREATE TABLE IF NOT EXISTS users(" +
-                    "id INT PRIMARY KEY AUTO_INCREMENT, " +
-                    "name VARCHAR(45) NOT NULL, " +
-                    "lastname VARCHAR(45) NOT NULL, " +
-                    "age INTEGER NOT NULL);");
+            statement.executeUpdate("" +
+                    "CREATE TABLE IF NOT EXISTS users(" +
+                    "id bigint NOT NULL AUTO_INCREMENT, " +
+                    "name varchar(45) NOT NULL, " +
+                    "lastName varchar(45) NOT NULL, " +
+                    "age tinyint NOT NULL, " +
+                    "PRIMARY KEY (id)) " +
+                    "ENGINE=InnoDB AUTO_INCREMENT = 1 DEFAULT CHARSET=utf8mb3");
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
     }
 
-    public void dropUsersTable() {
-        Connection connection = Util.getConnection();
-        try (Statement statement = connection.createStatement()) {
-            statement.executeUpdate("DROP TABLE IF EXISTS users");
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    public void saveUser(String name, String lastName, byte age) {
-        Connection connection = Util.getConnection();
-        try (PreparedStatement ps = connection.prepareStatement("INSERT INTO users VALUES(id, ?, ?, ?)")) {
-            ps.setString(1, name);
-            ps.setString(2, lastName);
-            ps.setByte(3, age);
-            ps.execute();
-            System.out.printf("User с именем – %s добавлен в базу данных%n", name);
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    public void removeUserById(long id) {
-        Connection connection = Util.getConnection();
-        try (PreparedStatement ps = connection.prepareStatement("DELETE FROM users WHERE ID = ?")) {
-            ps.setLong(1, id);
-            ps.executeUpdate();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    public List<User> getAllUsers() {
-        List<User> users = new ArrayList<>();
-        Connection connection = Util.getConnection();
-        try (PreparedStatement ps = connection.prepareStatement("SELECT * FROM users")) {
-            ResultSet resultSet = ps.executeQuery();
-            while (resultSet.next()) {
-                users.add(new User(resultSet.getString("name"), resultSet.getString("lastName"), resultSet.getByte("age")));
+        public void dropUsersTable () {
+            Connection connection = Util.getConnection();
+            try (Statement statement = connection.createStatement()) {
+                statement.executeUpdate("DROP TABLE IF EXISTS users");
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
             }
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
         }
-        return users;
+
+        public void saveUser (String name, String lastName,byte age){
+            Connection connection = Util.getConnection();
+            try (PreparedStatement ps = connection.prepareStatement("INSERT INTO users VALUES(id, ?, ?, ?)")) {
+                ps.setString(1, name);
+                ps.setString(2, lastName);
+                ps.setByte(3, age);
+                ps.execute();
+                System.out.printf("User с именем – %s добавлен в базу данных%n", name);
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        public void removeUserById ( long id){
+            Connection connection = Util.getConnection();
+            try (PreparedStatement ps = connection.prepareStatement("DELETE FROM users WHERE ID = ?")) {
+                ps.setLong(1, id);
+                ps.executeUpdate();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        public List<User> getAllUsers () {
+            List<User> users = new ArrayList<>();
+            Connection connection = Util.getConnection();
+            try (PreparedStatement ps = connection.prepareStatement("SELECT * FROM users")) {
+                ResultSet resultSet = ps.executeQuery();
+                while (resultSet.next()) {
+                    users.add(new User(resultSet.getString("name"), resultSet.getString("lastName"), resultSet.getByte("age")));
+                }
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+            return users;
+        }
+
+        public void cleanUsersTable () {
+            Connection connection = Util.getConnection();
+            try (Statement statement = connection.createStatement()) {
+                statement.executeUpdate("TRUNCATE TABLE users");
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 
-    public void cleanUsersTable() {
-        Connection connection = Util.getConnection();
-        try (Statement statement = connection.createStatement()) {
-            statement.executeUpdate("TRUNCATE TABLE users");
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-    }
-}
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -5,7 +5,7 @@ import java.sql.*;
 public class Util {
     private static final String URL = "jdbc:mysql://localhost:3306/users";
     private static final String USERNAME = "root";
-    private static final String PASSWORD = "gFhjkm@024";
+    private static final String PASSWORD = "root";
     public static Connection connection;
 
     public static Connection getConnection() {
